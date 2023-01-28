8a9d8ebd987738a724e2b75534772e883a722220
SlowyZo <122505870+Slowyzo@users.noreply.github.com>
2023-01-27 22:47:46 -0600

    Finished basic .js, updated pseudocode and readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rock-paper-scissors
+
+This is my first time coding something.
+Understanding how to go about the process was difficult,
+but using pseudocode to break down the problem was very helpful.
+
+Part1 
+I still needed to look up several things.. when to use let vs var, how to compare the strings, how to get the result of the function into console, etc.
+
+I didn't copy any whole solution but used bits and pieces, especially some 
+from old assinments, and also looked up answers to this problem directly 
+at times. 
--- javascript.js
+++ javascript.js
@@ -1,32 +1,40 @@
+/* Collect user choice from prompt and convert to lowercase */
+
+const userChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+console.log(userChoice)
 
 /* Generate computer choice randomly*/
-let randomNumber = Math.floor(Math.random()*100) + 1;
-console.log(randomNumber)
 
-let computerChoice;
+let randomNumber = Math.floor(Math.random()*100) + 1;
+
+var computerChoice;
 if (randomNumber <= 33) {
     computerChoice = 'rock'
 }
-
 else if (randomNumber >= 67) {
     computerChoice = 'paper'
 }
-
 else computerChoice = 'scissors';
 
 console.log(computerChoice)
 
-/* Collect user choice from prompt and convert to lowercase */
-const userChoice = prompt("Rock, Paper, or Scissors").toLowerCase();
+/* Compare user and computer choice */
 
-console.log(userChoice)
-
-/* Computer user and computer choice (not working) */
-let result;
-function compareChoice () {
-    if(userChoice === computerChoice)
-    result = 'you tied!';
+function compareChoice(choice1, choice2) {
+    if (choice1 === choice2) {return 'Tie!'}
+    else if (choice1 === 'rock')
+        {if (choice2 === 'scissors') {return 'You win!'} 
+        else {return 'Computer wins!'}}
+    else if (choice1 === 'paper') 
+        {if (choice2 === 'rock'){return 'You win!'} 
+        else {return 'Computer wins!'}}
+    else (choice1 === 'scissors') 
+        {if (choice2 === 'paper'){return 'You win!'} 
+        else {return 'Computer wins!'}};
 }
 
-/* Print result to console */
+/* Generate result and print to console */
+
+var result = compareChoice(userChoice, computerChoice);
 console.log(result)
