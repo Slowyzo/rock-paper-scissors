7f55e2e7b7c80debad0626d5e7db7a0d6c6edeef
SlowyZo <122505870+Slowyzo@users.noreply.github.com>
2023-02-08 21:38:25 -0600

    Adjusted scorekeeping, [200~nested components of rps into one function

--- javascript.js
+++ javascript.js
@@ -1,49 +1,68 @@
 
-let userScore = 0
-let computerScore = 0
 
 
-/* Collect user choice from prompt and convert to lowercase */
-function playRound(){
-const userChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-console.log(userChoice)
+/*let container = document.querySelector('.container');
 
-    /* Generate computer choice randomly*/
-    let randomNumber = Math.floor(Math.random()*100) + 1;
-        let computerChoice;
-        if (randomNumber <= 33){
-        computerChoice = 'rock'
+let rockbtn = document.querySelector('#rockbtn');
+rockbtn.addEventListener("click", () => {playRound('rock'); console.log('rock'); getScore();}); 
+
+let paperbtn = document.querySelector('#paperbtn');
+paperbtn.addEventListener("click", () => {playRound('paper'); console.log('paper'); getScore();}); 
+
+let scissorsbtn = document.querySelector('#scissorsbtn');
+scissorsbtn.addEventListener("click", () => {playRound('scissors'); console.log('scissors'); getScore();});   
+*/
+
+
+function rockPaperScissors(userChoice){
+
+    let userScore = 0;
+    let computerScore = 0;
+    let computerChoice = getComputerChoice();
+    let winner;
+    
+    
+    function getComputerChoice () {
+            let randomNumber = Math.floor(Math.random()*100) + 1;
+                if (randomNumber <= 33){return 'rock'
+                }
+                else if (randomNumber >= 67){return 'paper'
+                }
+                else {return 'scissors'}
         }
-        else if (randomNumber >= 67){
-            computerChoice = 'paper'
+    
+    
+    function playRound (userChoice, computerChoice) { 
+            if (userChoice === computerChoice){return winner = 'Tie!'}
+            else if (userChoice === 'rock')
+                {if (computerChoice === 'scissors'){return winner = 'You win!'} 
+                else {return winner = 'Computer wins!'}}
+            else if (userChoice === 'paper') 
+                {if (computerChoice === 'rock'){return winner = 'You win!'} 
+                else {return winner = 'Computer wins!'}}
+            else (userChoice === 'scissors') 
+                {if (computerChoice === 'paper'){return winner = 'You win!'} 
+                else {return winner = 'Computer wins!'}};
         }
-        else computerChoice = 'scissors';
-        console.log(computerChoice)
-
-    /* Compare user and computer choice */
-    function compareChoice(choice1, choice2){
-        if (choice1 === choice2){return 'Tie!'}
-        else if (choice1 === 'rock')
-            {if (choice2 === 'scissors'){return 'You win!'} 
-            else {return 'Computer wins!'}}
-        else if (choice1 === 'paper') 
-            {if (choice2 === 'rock'){return 'You win!'} 
-            else {return 'Computer wins!'}}
-        else (choice1 === 'scissors') 
-            {if (choice2 === 'paper'){return 'You win!'} 
-            else {return 'Computer wins!'}};
+    
+    
+        function getScore(winner) {
+            if (winner === 'Computer wins!'){computerScore++; 
+                return computerScore;}
+            else if (winner === 'You win!'){userScore++;
+                return userScore;}
+            else return;
         }
-
-/* Generate result and **SEND IT OUT OF NESTED FUNCTION!!*/
-return compareChoice(userChoice, computerChoice);
-
-}
-
-const rockbtn = document.querySelector('#rockbtn');
-rockbtn.addEventListener("click", playRound(rock));
-
-
-
-game ()
-console.log(userScore)
-console.log(computerScore)
+    
+        console.log(userChoice);
+        console.log(computerChoice);
+        playRound(userChoice, computerChoice)
+        console.log(winner);
+        getScore(winner);
+        console.log('Computer:' + computerScore);
+        console.log('User:' + userScore);
+    
+    }
+    
+    rockPaperScissors('rock');
+    
